2e5ebb77a6aeaef3d751c10a0adcbf37d6b69330
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-03 16:03:11 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userName, userEmail) {
+
+return fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    body: JSON.stringify({
+        name: userName, 
+        email: userEmail,
+    })
+})
+    .then(res => res.json())
+    .then(info => document.body.append(info.id))
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        document.body.append(error.message);
+    });
+}
